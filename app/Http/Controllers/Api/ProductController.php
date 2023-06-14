20e6b0a84427024b7581ecd9fd61f8be25d99ff1
nuguntu <nskhiereli@gmail.com>
2023-06-14 08:56:53 +0400

    feat: filter functionality

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,13 +7,33 @@ use App\Http\Requests\StoreProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Image;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return response()->json(ProductResource::collection(Product::latest()->get()));
+        if (!request()->has('product_name')) {
+
+            return response()->json(ProductResource::collection(Product::latest()->get()));
+        }
+
+
+        $products = Product::query()
+            ->when(!empty(request()->product_name), function ($query) {
+                $query->where('name', 'like', request()->product_name . '%');
+            })
+            ->when(!empty(request()->category_name), function ($query) {
+                $categoryId = ProductCategory::where('name', request()->category_name)->first()['id'];
+                $query->whereJsonContains('product_categories_id', $categoryId);
+            })->when(!empty(request()->price), function ($query) {
+                $query->where('price', '<=', request()->price);
+            })->when(!empty(request()->description), function ($query) {
+                $query->where('description',  'like',  request()->description . '%');
+            })->get();
+
+        return response()->json(ProductResource::collection($products));
     }
     public function destroy($productId)
     {
@@ -23,12 +43,9 @@ class ProductController extends Controller
     public function store(StoreProductRequest $request)
     {
         $categoryArray = [];
-
         for ($i = 0; $i < count($request->product_categories_id); $i++) {
-
             array_push($categoryArray, intval($request->product_categories_id[$i]));
         }
-
 
         $product = Product::create([
             'product_categories_id' => $categoryArray,
