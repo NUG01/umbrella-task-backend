ffeb6351f53394faa5438fd82db981956644a906
nuguntu <nskhiereli@gmail.com>
2023-06-14 11:19:31 +0400

    readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        ProductCategory::create([
+            'name' => 'Category',
+        ]);
+
+        for ($i = 0; $i < 10000; $i++) {
+            $product =  Product::create([
+                'name' => 'Name',
+                'description' => 'description',
+                'price' => $i * 2,
+                'product_categories_id' => [1],
+            ]);
+            Image::create([
+                'product_id' => $product->id,
+                'path' => 'assets/umbrella.jpg',
+            ]);
+        }
     }
 }
